2a26aedece74d5dbf93e596911bab66b92d816d1
user-synax <usersynax@gmail.com>
2026-06-08 09:04:05 +0530

    Removed: powersheel script

--- commit-each-file.ps1
+++
@@ -1,9 +0,0 @@
-$files = git ls-files --others --modified --exclude-standard
-$i = 1
-foreach ($file in $files) {
-    Write-Host "Committing file $i/101: $file"
-    git add "$file"
-    git commit -m "Add/Update: $file"
-    $i++
-}
-Write-Host "All 101 commits completed!"
