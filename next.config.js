e1f0a33b90862512147d41a27328c3bb0a65daa8
user-synax <usersynax@gmail.com>
2026-06-10 09:11:32 +0530

    Next Config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["mongoose"],
-  },
+  serverExternalPackages: ["mongoose"],
 };
 
 module.exports = nextConfig;
