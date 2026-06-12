777c84ee78321b64c1c9e52bd5fad37bcc26529f
user-synax <usersynax@gmail.com>
2026-06-12 18:53:31 +0530

    NA

--- commit-script.ps1
+++
@@ -1,8 +0,0 @@
-$modifiedFiles = git diff --name-only
-$untrackedFiles = git ls-files --others --exclude-standard
-$allFiles = $modifiedFiles + $untrackedFiles
-
-foreach ($file in $allFiles) {
-    git add $file
-    git commit -m "Add $file"
-}
