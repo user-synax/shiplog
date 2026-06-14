cfaed2ead7c21a8cf53bb69f119a2709523e508a
user-synax <usersynax@gmail.com>
2026-06-14 10:07:26 +0530

    robots.js for SEO

--- app/robots.js
+++ app/robots.js
@@ -1,12 +1,12 @@
 export default function robots() {
-  return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: ["/dashboard/", "/admin/", "/api/"],
-      },
-    ],
-    sitemap: "https://shiplog.usersynax.dev/sitemap.xml",
-  };
+    return {
+        rules: [
+            {
+                userAgent: "*",
+                allow: "/",
+                disallow: ["/dashboard/", "/admin/", "/api/"],
+            },
+        ],
+        sitemap: "https://shiplog.usersynax.dev/sitemap.xml",
+    };
 }
